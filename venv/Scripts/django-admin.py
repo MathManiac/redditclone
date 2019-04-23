c587d81318e092f10b0603e04e783ef6155f1a46
Henrik Hansen <lianides@hotmail.com>
2019-04-23 22:45:33 +0200

    hh

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!g:\programmering\projects\python\django\redditclone\redditclone\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
